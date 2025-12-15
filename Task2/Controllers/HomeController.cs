454ea167db8f8be07b226709be1332a3ca51f7cc
Cavidan <cavidanmirzeyev909@gmail.com>
2025-12-15 22:41:52 +0400

    first commit

---
+++ Task2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task2/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task2.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SinglePageDetail()
+        {
+            return View();
+        }
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
